e77a44561a1c5f363c5418dfab8ac9f5efa2843d
John Refior <jrefior@dispatch.me>
2019-08-27 20:30:02 -0400

    First try

---
+++ hider/image.go
@@ -0,0 +1,18 @@
+package hider
+
+func HideMessage(msg string, jpg []byte) []byte {
+	return append(jpg, []byte(msg))
+}
+
+func ReadMessage(jpg []byte) string {
+	var start int
+	for x := len(jpg) - 2; x > 0; x = x - 1 {
+		if jpg[x] == 0xFF && jpg[x+1] == 0xD9 {
+			start = x + 2
+			break
+		}
+	}
+	if start > len(jpg) {
+		return string(jpg[start:])
+	}
+}
